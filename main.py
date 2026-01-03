8a4ef063b3bf45a12cf95bc4aae586bd26da0296
Birdgem <30490765+Birdgem@users.noreply.github.com>
2026-01-03 22:13:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает ✅")
+
+
+def main():
+    print("RUNNING FILE:", __file__)
+
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
